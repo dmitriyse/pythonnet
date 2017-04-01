b13bfbfd4022039096943472303a467210d5d87a
dse <dse@sys-plex.com>
2017-04-01 16:22:08 +0400

    PythonWrapper added, default OS-dependend python compile constants added. (Py3.5m)

---
+++ src/Python.Plus.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Python.Net.Tests
+{
+    using System;
+    using System.Reflection;
+
+    using NUnit.Common;
+
+    using NUnitLite;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+        }
+    }
+}
